90acf755a9f108b89c13475abc98319e425e1dd0
MTV Mtv28 <meremkulovtv@gmail.com>
2024-07-10 20:39:02 +0300

    rt

--- handlers/reply_keyboard_handlers.py
+++ handlers/reply_keyboard_handlers.py
@@ -5,7 +5,7 @@ router = Router()
 
 
 @router.message(F.text == 'Адрес')
-async def adress(message: Message):
+async def address(message: Message):
     await message.answer('https://yandex.ru/maps/1104/cherkessk/house/'
                          'prospekt_lenina_1/YEsYdgNoSUUHQFpufX5yc3pqYQ==/?ll=42.048001%2C44.232695&z=17')
 
