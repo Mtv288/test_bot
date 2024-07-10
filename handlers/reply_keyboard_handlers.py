51a8c7307a5a5889c560c96b59c2ea67af883e7d
MTV Mtv28 <meremkulovtv@gmail.com>
2024-07-10 19:15:27 +0300

    we

--- handlers/reply_keyboard_handlers.py
+++ handlers/reply_keyboard_handlers.py
@@ -1,1 +1,10 @@
-imp
+from aiogram import F, Router, Bot, types
+from aiogram.types import Message, InputFile, input_media
+
+
+router = Router()
+
+@router.message(F.text == 'Адрес')
+async def adress(message: Message):
+    await message.answer('https://yandex.ru/maps/1104/cherkessk/house/'
+                         'prospekt_lenina_1/YEsYdgNoSUUHQFpufX5yc3pqYQ==/?ll=42.048001%2C44.232695&z=17')
